55b79770f98985fd42284927740b40f7eac15967
Jason Carmichael <jasonc0308@icloud.com>
2022-10-14 14:27:44 -0500

    displaying data

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 console.log('working')
 
 let button = document.querySelector('#searchButton')
+
+
+async function getData (event) {
+    event.preventDefault()
+    let textInput = document.querySelector('#inputBar').value
+
+    fetch(`https://poetrydb.org/author/${textInput}`)
+    .then(res => {
+        return res.json()
+    })
+    .then(res =>{
+        console.log('success!', res)
+        let poetName = document.querySelector('#poetName')
+        poetName.innerText = res[1].author
+
+        let poemTitle = document.querySelector('#title')
+        poemTitle.innerText = res[1].title
+
+        let poem = document.querySelector('#poem')
+        poem.innerText = res[1].lines
+
+        let numPoems = document.querySelector('#numPoems')
+        numPoems.innerText = `${res.length} poems available`
+    })
+    .catch(err =>{
+        console.log('error!', err)
+    })
+}
+
+
+// 1 attach Event to Button
+button.addEventListener('click', getData)
