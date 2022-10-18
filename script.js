48ec6657fad9ba87ef358498b799cf498ba19ad9
Jason Carmichael <jasonc0308@icloud.com>
2022-10-18 16:59:00 -0500

    stanza break

--- script.js
+++ script.js
@@ -20,37 +20,23 @@ async function getData (event) {
         let poemTitle = document.querySelector('#title')
         poemTitle.innerText = res[1].title
 
-        // let poem = document.querySelector('#poem')
-        // poem.innerText = res[1].lines
-        // this code is woking to display poem as <p>
-
         let numPoems = document.querySelector('#numPoems')
         numPoems.innerText = `${res.length} poems available`
 
         let poem = document.querySelector('#poem')
         poem.innerText = res[1].title
-        // the above code works
-
-        // const addLine = res[1].lines.forEach((x) => {
-            
-        // })
-
         
         for (let i = 0; i < res[1].lines.length; i++) {
-            // let poem = document.querySelector('#poem')
-            // poem.innerText = res[1].title
             let poemLine = document.createElement('li')
             poem.appendChild(poemLine)
-            poemLine.innerText = res[1].lines[i]
-            // if (res[1].lines[i] == "") {
-            //     poem.innerText = " "
-            // } else {
-            //     poemLine.innerText = res[1].lines[i]
-            // }
-                
+            // poemLine.innerText = res[1].lines[i]
             
-
-
+            if (res[1].lines[i] !== "") {
+                poemLine.innerText = res[1].lines[i]
+            } else {
+                poemLine.innerText = "-"
+            } // Why won't this display unless i put a character? Not even a space???
+           
             console.log(res[1].lines[i])
         }
         
