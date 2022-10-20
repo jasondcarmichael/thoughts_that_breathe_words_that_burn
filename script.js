2b7c2945ee27203d7cf5b7a4bdd0ce6198389d9f
Jason Carmichael <jasonc0308@icloud.com>
2022-10-20 09:58:30 -0500

    restructured HTML

--- script.js
+++ script.js
@@ -14,23 +14,18 @@ async function getData (event) {
     })
     .then(res =>{
         console.log('success!', res)
-        let poetName = document.querySelector('#poetName')
-        poetName.innerText = `By ${res[1].author}`
-
+        
         let poemTitle = document.querySelector('#title')
         poemTitle.innerText = res[1].title
 
-        // let numPoems = document.querySelector('#numPoems')
-        // numPoems.innerText = `${res.length} poems available`
+        let poetName = document.querySelector('#poetName')
+        poetName.innerText = `By ${res[1].author}`
 
         let poem = document.querySelector('#poem')
-        //poem.innerText = res[1].title
         
         for (let i = 0; i < res[1].lines.length; i++) {
-            let poemLine = document.createElement('li')
+            let poemLine = document.createElement('div')
             poem.appendChild(poemLine)
-            //poemLine.innerText = res[1].lines[i]
-            
             if (res[1].lines[i] !== "") {
                 poemLine.innerText = res[1].lines[i]
             } else {
@@ -41,10 +36,7 @@ async function getData (event) {
             console.log(res[1].lines[i])
         }
         
-            
-            
-
-       
+              
     })
     .catch(err =>{
         console.log('error!', err)
@@ -54,6 +46,3 @@ async function getData (event) {
 
 // 1 attach Event to Button
 button.addEventListener('click', getData)
-
-
-// Poems stored in [lines] array . Each line of poem is an index in [lines]. Create a <li> for each index in the array [lines]. Just map() to new array? Or forEach()?
